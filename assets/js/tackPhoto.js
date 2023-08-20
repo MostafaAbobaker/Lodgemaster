e600c564d4e42b454d302f4a54f253d418a15e56
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2023-08-21 02:22:07 +0300

    Add Button Camera

--- assets/js/tackPhoto.js
+++ assets/js/tackPhoto.js
@@ -1,14 +1,30 @@
-document.addEventListener("DOMContentLoaded", (ev) => {
-  //get the captured media file
-  let input = document.getElementById("capture");
+const buttonCamera = dropArea.querySelector(".TakePhotos");
 
-  input.addEventListener("change", (ev) => {
-    console.dir(input.files[0]);
-    if (input.files[0].type.indexOf("image/") > -1) {
-      img.src = window.URL.createObjectURL(input.files[0]);
-      let img = `<img src="${img.src}" alt="">`;
-      const dropArea = document.querySelector("#dragArea");
-      dropArea.innerHTML = imgTag;
-    }
-  });
+buttonCamera.onclick = () => {
+  inputCam.click(); //if user click on the button then the input also clicked
+};
+
+//get the captured media file
+let inputCam = document.getElementById("capture");
+
+inputCam.addEventListener("change", (ev) => {
+  console.dir(inputCam.files[0]);
+  if (inputCam.files[0].type.indexOf("image/") > -1) {
+    let img = document.getElementById("img");
+    img.src = window.URL.createObjectURL(inputCam.files[0]);
+    img.style.display = "block";
+  }
+
+  //  console.dir(input.files[0]);
+  //  if (input.files[0].type.indexOf("image/") > -1) {
+  //    let img = `<img src="" alt="">`;
+  //    img.src = window.URL.createObjectURL(input.files[0]);
+  //    dropArea.innerHTML = img;
+  //  } else if (input.files[0].type.indexOf("audio/") > -1) {
+  //    let audio = document.getElementById("audio");
+  //    audio.src = window.URL.createObjectURL(input.files[0]);
+  //  } else if (input.files[0].type.indexOf("video/") > -1) {
+  //    let video = document.getElementById("video");
+  //    video.src = window.URL.createObjectURL(input.files[0]);
+  //  }
 });
